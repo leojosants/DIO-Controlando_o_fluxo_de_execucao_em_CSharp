453f76ff7405a081408c949f24e4ed70338a7690
Leonardo Santos <leonardojosantos@gmail.com>
2022-09-07 16:26:11 -0300

    Pulando com BREAK e CONTINUE

---
+++ Aula04/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Boolean condicao = true;
+int valor;
+
+do
+{
+    Console.Write("\nDigite um valor, 0 para sair: ");
+    valor = int.Parse(Console.ReadLine());
+
+    if (valor == 0)
+    {
+        Console.WriteLine("Você saiu da aplicação.");
+        break;
+    }
+    else
+    {
+        Console.WriteLine($"Valor digitado: {valor}");
+        continue;
+    }
+
+} while (condicao);
+
+Console.Write("\n");
