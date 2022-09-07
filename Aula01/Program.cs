1c0e5129df33b964c12011e6a269ca348d9172ed
Leonardo Santos <leonardojosantos@gmail.com>
2022-09-07 14:16:27 -0300

    Capturando Entradas do Usuário pelo Console

---
+++ Aula01/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+
+namespace Aula01
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor1;
+            int valor2;
+            int total;
+
+            Console.Write("\nDigite o primeiro valor: ");
+            valor1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            valor2 = int.Parse(Console.ReadLine());
+
+            total = valor1 + valor2;
+
+            Console.WriteLine($"\nPrimeiro valor digitado foi: {valor1}");
+            Console.WriteLine($"O segundo valor digitado foi: {valor2}");
+            Console.WriteLine($"A soma dos valores {valor1} e {valor2} é: {total}");
+
+            Console.Write("\n");
+        }
+
+    } // fim class Program
+
+} // fim namespace Aula01
